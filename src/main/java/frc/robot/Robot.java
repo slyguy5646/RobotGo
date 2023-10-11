5b2f417b777511fbf3065d34508ecade9e393f7d
slyguy5646 <lpmonaghan06@yahoo.com>
2023-10-11 11:49:48 +0800

    added basic code to robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,6 +20,8 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  private Motor motor;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -29,6 +31,8 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    motor = new Motor();
   }
 
   /**
@@ -78,7 +82,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    motor.spinMotor(0.1); // set to 10% power (try changing this value!)
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
